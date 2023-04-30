86667da6ad978c97c0d11afd4d61bb45c6714e5d
Joe Mei <mjoe27798@gmail.com>
2023-04-29 23:58:03 -0400

    12am

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from make_draggable import make_draggable
 from calculator import *
 from stopwatch import *
 from timer import *
-from to_do_list import *
+from to_do_list import To_Do_List
 
 #Initialize Window
 
@@ -18,6 +18,7 @@ root.geometry('1280x720')
 root.title("Your study spacce")
      
 #Frontend part of code - Interface
+
 stickyNoteWidget = Frame(root, bd = 4, bg = 'white')
 stickyNoteWidget.place(x=10, y=20)
 make_draggable(stickyNoteWidget)
@@ -27,7 +28,9 @@ notes.pack()
 
 #weatherWidget = weather_widget(root)
 stopwatchWidget = Stopwatch(root)
+todolist = To_Do_List(root)
 calculatorWidget = Calculator(root)
 timerWidget = Timer(root)
+
 
 root.mainloop()
