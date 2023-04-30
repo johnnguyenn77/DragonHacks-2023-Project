9d5103df045b9dbfdf611e8b63dbf05daddf9f58
John Nguyen <nguyenhoangtu.427@gmail.com>
2023-04-30 05:58:50 -0400

    to-do list format changes

--- to_do_list.py
+++ to_do_list.py
@@ -5,19 +5,23 @@ from make_draggable import make_draggable
 class To_Do_List:
 
     def __init__(self,root):
-        self.frame = tkinter.Frame(root, bd = 4, bg ='#a7bfd5')
+        self.frame = tkinter.Frame(root, highlightbackground='white', highlightcolor= "white",highlightthickness=1, bg ='#a7bfd5')
         make_draggable(self.frame)
         
         #Where GUI starts
+        name = tkinter.Label(self.frame, text = 'To-do List', font = ("Segoe UI Variable Display", 10), bg = '#a7bfd5')
+        close_btn = tkinter.Button(self.frame, bd = 0, width = 3, text = 'X', command = self.frame.place_forget, bg = '#cedfef', font = ("Segoe UI Variable Display", 12))
+        name.pack(anchor = 'nw', pady=4)
+        close_btn.place(x= 290, y = 0)
         frame_tasks = tkinter.Frame(self.frame)
 
         frame_tasks.pack()
 
-        self.listbox_tasks = tkinter.Listbox(frame_tasks,height=10,width=50, bg = '#cedfef')
+        self.listbox_tasks = tkinter.Listbox(frame_tasks,height=10,width=50, bg = '#cedfef', bd = 0)
         self.listbox_tasks.pack(side=tkinter.LEFT)
 
 
-        scrollbar_tasks = tkinter.Scrollbar(frame_tasks, bg = '#cedfef')
+        scrollbar_tasks = tkinter.Scrollbar(frame_tasks, bg = '#cedfef', bd = 0)
         scrollbar_tasks.pack(side = tkinter.RIGHT, fill = tkinter.Y)
 
         self.listbox_tasks.config(yscrollcommand=scrollbar_tasks.set)
@@ -26,10 +30,10 @@ class To_Do_List:
         self.entry_task = tkinter.Entry(self.frame, width=53, bg = '#cedfef')
         self.entry_task.pack()
 
-        button_add_task = tkinter.Button(self.frame, text="Add task", width=10, command=self.add_task, bg = '#cedfef')
+        button_add_task = tkinter.Button(self.frame,bd = 0,text="Add task", width=10, command=self.add_task, bg = '#cedfef')
         button_add_task.pack(side = tkinter.LEFT)
 
-        button_delete_task = tkinter.Button(self.frame, text="Delete task", width=10, command=self.delete_task, bg = '#cedfef')
+        button_delete_task = tkinter.Button(self.frame, bd = 0,text="Delete task", width=10, command=self.delete_task, bg = '#cedfef')
         button_delete_task.pack(side = tkinter.RIGHT)
         
         self.load_tasks()
@@ -81,6 +85,9 @@ class To_Do_List:
 
 #Create GUI
 
-#root = tkinter.Tk()
-#todolist = To_Do_List(root)
-#root.mainloop()
+root = tkinter.Tk()
+root.title("Time Counter")
+root.geometry("1280x720")
+root.configure(bg='#222222')
+todolist = To_Do_List(root)
+root.mainloop()
