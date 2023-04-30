b44b9e763b442a43e0f1647bff0478ccc7030661
John Nguyen <nguyenhoangtu.427@gmail.com>
2023-04-30 06:19:42 -0400

    changes

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from tkinter import *
-import json
 from datetime import datetime
 import pip._vendor.requests as requests
 import time
 from weather import weather_widget
-#from calculator import *
 from stopwatch import *
 from timer import *
 from to_do_list import To_Do_List
@@ -12,7 +10,6 @@ from stickynote import Sticky_Note
 from clock import DigitalClock
 from Sidebar import *
 from calendar_1 import CALENDAR
-#from calendar_1 import CALENDAR
 
 #Initialize Window
 
@@ -22,7 +19,6 @@ root.geometry('1280x720')
 root.title("Your study space")
 
 def addWeather():
-    #parent = Sticky_Note.nametowidget(Sticky_Note.winfo_parent())
     weather2 = weather_widget(root)
 
 def addStickyNotes():
@@ -42,6 +38,7 @@ def addClock():
 
 def addCalendar():
     calender = CALENDAR(root)
+    
 #Frontend part of code - Interface
 
 sb = Sidebar(root)
@@ -53,19 +50,5 @@ sb.add_button('Stopwatch', command=addStopwatch)
 sb.add_button('To-do List', command=addToDoList)
 sb.add_button('Clock', command=addClock)
 sb.add_button('Calendar', command=addCalendar)
-
-# stickyNoteWidget = Frame(root, bd = 4, bg = 'white')
-# stickyNoteWidget.place(x=10, y=20)
-# make_draggable(stickyNoteWidget)
-
-# notes = Text(stickyNoteWidget)
-# notes.pack()
-
-#weatherWidget = weather_widget(root)
-#topwatchWidget = Stopwatch(root)
-#todolist = To_Do_List(root)
-#calculatorWidget = Calculator(root)
-#timerWidget = Timer(root)
-#stickynote = Sticky_Note(root)
 
 root.mainloop()
